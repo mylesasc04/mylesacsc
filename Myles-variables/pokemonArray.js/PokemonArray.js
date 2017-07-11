a49b7ae6faa6d889ff3fcb13b854530cfa87ab16
mylesasc4 <mylesduah123@gmail.com>
2017-07-11 16:32:12 -0400

    First Commit

---
+++ Myles-variables/pokemonArray.js/PokemonArray.js
@@ -0,0 +1,6 @@
+var myPokemonRoster = ["pikachu","charizard","Dragonite","Golem"]
+console.log(myPokemonRoster[1])
+myPokemonRoster.length
+for(i=0; i<myPokemonRoster.length; i++)
+{
+    console.log(myPokemonRoster);}
---
+++ Myles-variables/pokemonVariable.js/Pokemon.js
@@ -0,0 +1,12 @@
+var DragoniteHP = 95;
+var DragoniteNum = 103;
+var DragoniteType = "Dragon and Flying";
+var DragoniteBestMove = "Dragon Breath";
+var DragoniteLastEvo = true;
+var DragoniteAttack = 267
+var DragoniteDefense = 500
+var DragoniteSpeed = 185
+
+Console.log(DragoniteHp)
+Console.log\
+Console.log
