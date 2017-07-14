46b8f1e683ec04cf095ff42ffbc39e6e55a79dd9
mylesasc4 <mylesduah123@gmail.com>
2017-07-13 20:02:45 -0400

    modified

--- buyLoop.js
+++ buyLoop.js
@@ -5,16 +5,17 @@ var prompt = require('prompt-sync')();
 var bank = 1000; // start the player with 1,000 dollars
 console.log('Welcome to your Adventure! Heres your cash, you have 1,000 dollars!!'); //tell the player 
 
-var inventory = ['hyrule sword?','potion?','shield?','bow?'];
+var inventory = ['hyrule sword? ','potion? ','shield? ','bow?'];
 
 //prompt the user if they want to buy an item
-while(bank>249)
+while(bank>0)
 for(var x =0; x < inventory.length; x++){
 var answer = prompt('would you like to purchase a ' + inventory[x]);
 if(answer === 'yes'|| answer === 'yes please'){
-    bank = bank - 250;
+    bank = bank - 100;
     console.log('you have ' + bank + ' left ')
-if(answer ==='no','no thanks'){
+var answer = prompt('would you like to purchase a ' + inventory[x]);
+if(answer ==='no'){
     console.log('okay how about these other Items?,')
 
 };
--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,7 +1,13 @@
-var myPokemonRoster
-[[Vaporeon
-charizard
-Dragonite
-Mewtwo
-Sylveon
-blastoise
+var myPokemonRoster=[
+    ["Vaporeon", "water", 310, 145, 128,false],
+    ["charizard","flying","fire", 310, 145, 128,true],
+    ["Dragonite","Flying","Dragon", 310, 145, 128,true],
+    ["Mewtwo","Pyschic", 310, 145, 128,true],
+    ["Sylveon","Fairy", 310, 145, 128,true],
+    ["blastoise","Water", 310, 145, 128,true]
+]
+
+function logPokemon(){ console.log('hello')
+
+}
+
